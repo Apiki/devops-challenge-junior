2cabb0575be3a6d926c3f97bf352200a58fb0e27
EduardoHCorrea <contatodoranlol@gmail.com>
2025-03-14 11:01:03 -0300

    Devops Challenge Junior Eduardo Henrique Correa ( Prints, URL e dados para login estao no comentario no final do codigo )

--- devops_challenge.php
+++ devops_challenge.php
@@ -1,86 +1,82 @@
+<?php
 /**
  * @package Devops_challenge_Junior
- * @version 1.0
+ * @version 1.1
  */
 /*
-Plugin Name: Devops challenge Júnior
+Plugin Name: Devops Challenge Júnior
 Plugin URI: https://apiki.com/
 Description: Sabe de nada, inocente! Ordinária!!
 Author: Apiki WordPress
-Version: 1.0
+Version: 1.1
 */
 
-$global_lyrics;
+global $global_lyrics;
+$global_lyrics = "Pau que nasce torto nunca se endireita\nMenina que requebra a mãe pega na cabeça\nPau que nasce torto nunca se endireita\nMenina que requebra a mãe pega na cabeça\nDomingo ela não vai (vai, vai)\nDomingo ela não vai não (vai, vai, vai)\nOlha, domingo ela não vai (vai, vai)\nDomingo ela não vai não (vai, vai, vai)\nO pau que nasce torto nunca se endireita\nMenina que requebra a mãe pega na cabeça\nPau que nasce torto nunca se endireita\nMenina que requebra a mãe pega na cabeça\nSegure o tchan\nAmarre o tchan\nSegure o tchan tchan tchan tchan\nDepois de nove meses você vê o resultado\nEsse é o Gera Samba arrebentando no pedaço\nJoga ela no meio, mete em cima, mete embaixo";
 
 function apiki_segura_o_tchan() {
-	$lyrics = $global_lyrics;
-	
-	$lyrics = "Pau que nasce torto nunca se endireita
-	Menina que requebra a mãe pega na cabeça
-	Pau que nasce torto nunca se endireita
-	Menina que requebra a mãe pega na cabeça
-	Domingo ela não vai (vai, vai)
-	Domingo ela não vai não (vai, vai, vai)
-	Olha, domingo ela não vai (vai, vai)
-	Domingo ela não vai não (vai, vai, vai)
-	O pau que nasce torto nunca se endireita
-	Menina que requebra a mãe pega na cabeça
-	Pau que nasce torto nunca se endireita
-	Menina que requebra a mãe pega na cabeça
-	Segure o tchan
-	Amare o tchan
-	Segure o tchan tchan tchan tchan
-	Depois de nove meses você vê o resultado
-	Esse é o Gera Samba arrebentando no pedaço
-	Joga ela no meio, mete em cima, mete embaixo";
-
-	$lyrics = explode( "\n", $lyrics )
-
-	return wptexturize( $lyrics[ mt_rand( count( $lyrics ) - 1, 0 ) ] );
+    global $global_lyrics; 
+    
+    $lyrics_array = explode("\n", $global_lyrics); 
+    
+    return wptexturize($lyrics_array[ mt_rand(0, count($lyrics_array) - 1) ]); 
 }
 
 function devops_challenge() {
-	$chosen = $undefined_variable;
-	$lang   = '';
-	if ( 'en_' !== substr( get_user_locale(), 0, 3 ) ) {
-		$lang = ' lang="en"';
-	}
-
-	printf(
-		'O texto é: %s %s %s',
-		__( 'Segure o Tchan, by Apiki WordPress:' ),
-		$lang
-	);
+    $chosen = apiki_segura_o_tchan(); 
+    $lang   = '';
+    if ( 'en_' !== substr( get_user_locale(), 0, 3 ) ) {
+        $lang = ' lang="en"';
+    }
+    
+    printf(
+        '<p id="devop">%s <span%s>%s</span></p>',
+        esc_html__( 'Segure o Tchan, by Apiki WordPress:', 'devops-challenge' ),
+        $lang,
+        esc_html( $chosen ) 
+    );
 }
 
-add_action( '', 'devops_challenge' );
+add_action( 'admin_notices', 'devops_challenge' );
 
 function devop_css() {
-	echo "
-	<style type='text/css'>
-	#devop {
-		float: right;
-		padding: 5px 10px;
-		margin: 0;
-		font-size: 12px;
-		line-height: 1.6666;
-	}
-	.rtl #devop {
-		float: left;
-	}
-	.block-editor-page #devop {
-		display: none;
-	}
-	@media screen and (max-width: 782px) {
-		#devop,
-		.rtl #devop {
-			float: none;
-			padding-left: 0;
-			padding-right: 0;
-		}
-	}
-	</style>
-	";
+    echo "
+    <style type='text/css'>
+    #devop {
+        float: right;
+        padding: 5px 10px;
+        margin: 0;
+        font-size: 12px;
+        line-height: 1.6666;
+    }
+    .rtl #devop {
+        float: left;
+    }
+    .block-editor-page #devop {
+        display: none;
+    }
+    @media screen and (max-width: 782px) {
+        #devop,
+        .rtl #devop {
+            float: none;
+            padding-left: 0;
+            padding-right: 0;
+        }
+    }
+    </style>
+    ";
 }
 
 add_action( 'admin_head', 'devop_css' );
+
+/* 
+   Acesso WordPress = http://35.172.49.99/wp-admin
+   Login = user
+   Senha = 1OCuKwp^xrqTe%!USGd^txAw 
+   Print WordPress1 : https://imgur.com/a/xe2SMs7
+   Print WordPress2: https://imgur.com/a/7r0IMJV
+   Print AmazonLightsail: https://imgur.com/a/wjT5sJj
+   Print AmazonLightsail 2: https://imgur.com/a/AUTnYXs
+   Plugin Ativado: https://imgur.com/a/cHOogpx
+
+   */
